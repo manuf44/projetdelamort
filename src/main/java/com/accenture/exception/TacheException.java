e00b119bf17a81f1e59a851c06ac29b2499fe9f3
Emmanuel Fernandez <emmanuel.fernandez@accenture.com>
2025-02-19 14:33:45 +0100

    Ajout d'un commentaire

--- src/main/java/com/accenture/exception/TacheException.java
+++ src/main/java/com/accenture/exception/TacheException.java
@@ -1,5 +1,9 @@
 package com.accenture.exception;
 
+
+/**
+ * Classe d'exception métier concernant les taches
+ */
 public class TacheException extends RuntimeException {
   public TacheException(String message) {
     super(message);
